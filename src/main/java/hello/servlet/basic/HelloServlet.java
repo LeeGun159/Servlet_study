e1ce65c868c0740fad51a4df11e0cee480eb7865
LeeGun159 <tdyfuh7536@naver.com>
2025-01-11 22:43:45 +0900

    -서블릿 프로젝트 생성

--- src/main/java/hello/servlet/basic/HelloServlet.java
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -2,14 +2,15 @@ package hello.servlet.basic;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
 @WebServlet(name = "helloServlet", urlPatterns = "/hello")
-public class HelloServlet {
-
+public class HelloServlet extends HttpServlet {
+    @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("Hello Servlet");
         System.out.println("request = " + request);
